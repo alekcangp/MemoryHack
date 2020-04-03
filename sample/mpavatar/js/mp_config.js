13172e0aa8707e778813a7c97aa5ba755cceef8f
alekcangp <40195312+alekcangp@users.noreply.github.com>
2020-04-03 08:38:32 +0600

    Update mp_config.js

--- sample/mpavatar/js/mp_config.js
+++ sample/mpavatar/js/mp_config.js
@@ -2,5 +2,5 @@ var MpConfig = {
     "serverURL" : "http://sdkserver.motionportrait.jp/synthesizer/",
     "id" : "alekcangplistru",
     "key" : "ctqwgk7wmz",
-    "documentPath" : "/sample/mpavatar/"
+    "documentPath" : "/MemoryHack/sample/mpavatar/"
 };
